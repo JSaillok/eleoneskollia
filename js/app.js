45fa6a1b5b0af8212088c26cda664bf73b057477
Ioannis Kollias <83131661+JSaillok@users.noreply.github.com>
2022-10-11 21:15:18 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,3 +11,28 @@ sr.reveal('.content', {origin: 'top'});
 sr.reveal('.maintitle', { delay: 500, origin: 'left'});
 sr.reveal('.about .image', { delay: 600, origin: 'bottom'});
 sr.reveal('.textbox', { delay: 700, origin: 'right'});
+sr.reveal('.containerContact',{ delay: 500, origin: 'bottom'});
+
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".banner");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("navscrolled");
+    } else {
+      header.classList.remove("navscrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
